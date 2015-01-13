757abf0640033b3c9d0b13dd8b97d0c97c1d33a8
brapastor <brapastor@gmail.com>
2015-01-13 12:28:40 -0500

    Agregado de tags

--- app/src/main/java/miscontactos/brapastor/com/miscontactos/MainActivity.java
+++ app/src/main/java/miscontactos/brapastor/com/miscontactos/MainActivity.java
@@ -5,21 +5,46 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TabHost;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import miscontactos.brapastor.com.miscontactos.util.Contacto;
 import miscontactos.brapastor.com.miscontactos.util.TextChangedListener;
 
 public class MainActivity extends ActionBarActivity {
 
     private EditText txtNombre, txtTelefono, txtEmail, txtDireccion;
     private Button btn_agregar;
+    private List<Contacto> contactos= new ArrayList<Contacto>();
+    private ListView contactsListView;
+    private TabHost tabHost;
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inicializarComponentesUI();
+        iniciarlizarTabs();
 
+    }
+
+    private void iniciarlizarTabs() {
+       tabHost =(TabHost) findViewById(R.id.tabHost);
+        tabHost.setup(); // decimos a android que las pestañas que los tags van a ser modificados
+
+        TabHost.TabSpec spec = tabHost.newTabSpec("tab1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Crear");
+        tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec("tab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Lista");
+        tabHost.addTab(spec);
     }
 
     private void inicializarComponentesUI() {
@@ -27,6 +52,7 @@ public class MainActivity extends ActionBarActivity {
         txtTelefono = (EditText) findViewById(R.id.cmpTelefono);
         txtEmail = (EditText) findViewById(R.id.cmpEmail);
         txtDireccion = (EditText) findViewById(R.id.cmpDireccion);
+        contactsListView = (ListView) findViewById(R.id.listView);
 
         txtNombre.addTextChangedListener(new TextChangedListener(){
             @Override
@@ -35,8 +61,6 @@ public class MainActivity extends ActionBarActivity {
                 btn_agregar.setEnabled(!seq.toString().trim().isEmpty());
             }
         });
-
-
     }
 
     public void onClick(View view) {
